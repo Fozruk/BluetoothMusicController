177aa861eaf658288010b5ebd05ca2465500d628
Philipp Hentschel <philipphentschel@t-online.de>
2016-06-05 12:52:00 +0000

    player_proxy gets returned

--- bluetoothcontroller.py
+++ bluetoothcontroller.py
@@ -14,18 +14,25 @@ def initdbus():
 
 def getCurrentPlayer(dbusSession):
 	mac_address = open(expanduser("~")+"/.config/.bluetoothplayer.conf").read().replace(":","_").replace("\n","")
-	#print mac_address
+	#lprint mac_address
 	players = ['player1','player2'] #player1= iOS Player, player2 = Spotify or 3rd party player
 	for player in players:
-		player_proxy = dbusSession.get_object("org.bluez","/org/bluez/hci0/dev_{}/{}".format(mac_address,player)) 
-		playercontrol = dbus.Interface(player_proxy,'org.bluez.MediaPlayer1')
-		if checkPlayerAvail(player_proxy):
-			print "Found Player - {}".format(getTrackInfo(player_proxy))
-			return playercontrol
+		current_player = dbusSession.get_object("org.bluez","/org/bluez/hci0/dev_{}/{}".format(mac_address,player)) 
+		playercontrol = getPlayer(current_player)
+		if checkPlayerAvail(current_player):
+			print "Found Player - {}".format(getTrackInfo(current_player))
+			return current_player
+
+def getPlayer(player_proxy):
+	return dbus.Interface(player_proxy,'org.bluez.MediaPlayer1')
 
 def getTrackInfo(player_proxy):
 	prop_mang = dbus.Interface(player_proxy,'org.freedesktop.DBus.Properties')
 	return prop_mang.Get('org.bluez.MediaPlayer1','Track')
+
+def getPlayerState(player_proxy):
+	prop_mang = dbus.Interface(player_proxy,'org.freedesktop.DBus.Properties')
+	return prop_mang.Get('org.bluez.MediaPlayer1','Status')
 
 def checkPlayerAvail(player_proxy):
 	try:
@@ -36,11 +43,14 @@ def checkPlayerAvail(player_proxy):
 		return False
 	return True
 
-def sendcommand(playercontrol,command):
-	if command == 'play':
-		playercontrol.Play()
-	elif command == 'pause':
-		playercontrol.Pause()
+def sendcommand(player_proxy,command):
+	playstring = getPlayerState(player_proxy)
+	playercontrol = getPlayer(player_proxy)
+	if command == 'play' or command == 'pause':
+		if playstring == 'playing':
+			playercontrol.Pause()
+		else:
+			playercontrol.Play()
 	elif command == 'next':
 		playercontrol.Next()
 	elif command == 'previous':
